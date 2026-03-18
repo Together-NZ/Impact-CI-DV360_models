8c9c148219b8f37489c1f0dee8681239866e17bb
Peter Wang <pw33537515@gmail.com>
2026-03-18 15:03:47 +1300

    fix partition on dv360 youtube

--- macros/dv360_youtube.sql
+++ macros/dv360_youtube.sql
@@ -25,9 +25,7 @@ WITH parsed_data AS (
                 FORMAT_DATE('%Y-%m-%d', safe.PARSE_DATE('%Y/%m/%d', JSON_VALUE(data, "$.Date"))), -- Use converted date
                 JSON_VALUE(data, "$.Insertion Order ID"),
                 JSON_VALUE(data, "$.Line Item ID"),
-                JSON_VALUE(data, "$.YouTube Ad ID"),
-                JSON_VALUE(data, "$.YouTube Ad"),
-                JSON_VALUE(data, "$.YouTube Ad Group ID")
+                JSON_VALUE(data, "$.YouTube Ad ID")
                 --safe_cast(TRUNC(SAFE_CAST(JSON_EXTRACT_SCALAR(data, "$['Revenue (Adv Currency)']") AS FLOAT64))as int64)
             ORDER BY 
                 JSON_EXTRACT_SCALAR(data, "$['Revenue (Adv Currency)']") DESC -- Keep the record with the highest revenue
